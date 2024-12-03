0c9b1eb521221bca8e857ca5c5affb6de4b86c96
GlassOnTin <63980135+GlassOnTin@users.noreply.github.com>
2024-12-03 16:11:01 +0000

    v0.4.5

--- bluep/__init__.py
+++ bluep/__init__.py
@@ -14,7 +14,7 @@ from .secure_config import SecureConfig
 from .session_manager import SessionData
 from .websocket_manager import WebSocketManager
 
-__version__ = "0.4.4"
+__version__ = "0.4.5"
 __all__ = [
     "RateLimitMiddleware",
     "SessionData",
